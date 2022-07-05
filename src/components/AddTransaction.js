82345fce4511fd6d891ddaff6e4628abf42077c3
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-04 19:16:01 -0500

    i literally missed an s

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,64 +1,27 @@
-// import React, { useState, useContext} from 'react'
-// import { GlobalContext } from '../context/GlobalState';
-
-// export const AddTransaction = () => {
-//     const[text, setText] = useState('');
-//     const[amount, setAmount] = useState(0);
-
-//     const { addTransaction } = useContext(GlobalContext)
-
-//     const onSubmit = e => {
-//       e.preventDefault()
-
-//       const newTransaction = {
-//         id: Math.floor(Math.random() * 10000000),
-//         text,
-//         amount: +amount
-//       }
-
-//       addTransaction(newTransaction)
-//     }   
-//   return (
-//     <>
-//       <h3>Add new transaction</h3>
-//       <form onSubmit={onSubmit}>
-//         <div className="form-control">
-//           <label htmlFor="text">Text</label>
-//           <input type="text" value={text} onChange={(e)=> setText(e.target.value)}placeholder="Enter text..." />
-//         </div>
-//         <div className="form-control">
-//           <label htmlFor="amount"
-//             >Amount <br />
-//             (negative - expense, positive - income)</label
-//           >
-//           <input type="number" value={amount} onChange={(e)=> setAmount(e.target.value)}placeholder="Enter amount..." />
-//         </div>
-//         <button className="btn">Add transaction</button>
-//       </form>
-//     </>
-//   )
-// }
-
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
-  const [text, setText] = useState('');
-  const [amount, setAmount] = useState(0);
+    const[text, setText] = useState('');
+    const[amount, setAmount] = useState(0);
 
-  const { addTransaction } = useContext(GlobalContext);
+    const { addTransaction } = useContext(GlobalContext);
 
-  const onSubmit = e => {
-    e.preventDefault();
+    const onSubmit = e => {
+      e.preventDefault()
 
-    const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
-      text,
-      amount: +amount
+
+
+      const newTransaction = {
+        id: Math.floor(Math.random() * 10000000),
+        text,
+        amount: +amount
+      }
+
+      addTransaction(newTransaction);
+
+      console.log(newTransaction)
     }
-
-    addTransaction(newTransaction);
-  }
 
   return (
     <>
@@ -66,17 +29,18 @@ export const AddTransaction = () => {
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e)=> setText(e.target.value)}placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e)=> setAmount(e.target.value)}placeholder="Enter amount..." />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className="btn" type='submit'>Add Transaction</button>
       </form>
     </>
   )
 }
+
