f339b6436338371ec3422c9745ccad3ec31177f7
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-06 18:33:40 -0500

    get request

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction')
+
 // @desc Get all transactions
 //@route GET /api/v1/transactions
 //@access Public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions')
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find()
+
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        })
+    } catch (err) {
+        return res.send(500).json({
+            success: false,
+            error: 'Server error'
+        })
+    }
 }
 
 // @desc add transactions
 //@route POST /api/v1/transactions
 //@access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
     res.send('POST transaction')
 }
 
 // @desc delete transactions
 //@route delete /api/v1/transactions/:id
 //@access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE transaction')
 }
