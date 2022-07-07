b40b33d0f3b262595ea2fce60959d569efe47eaf
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-06 19:00:32 -0500

    post request done

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ connectDB();
 
 const transactions = require('./routes/transactions')
 
-const app = express()
 
+
+const app = express()
+app.use(express.json())
 app.use('/api/v1/transactions', transactions)
 
 const PORT = process.env.PORT || 5000
